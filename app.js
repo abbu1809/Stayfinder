db456c09ad51d296de7841d088ef61a300af396a
abbu1809 <abhishekkumarjaiswal9722@gmail.com>
2025-08-12 15:50:49 +0530

    feat: add validation and error handling for listings, implement async wrapper, and enhance edit form feedback

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const Listing = require('./models/listing');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-
+const wrapAsync = require("./utils/wrapasync");
+const ExpressError = require('./utils/ExpressError');
+const {listingSchema} = require('./schema.js');
 
 const MONGO_URL= 'mongodb://127.0.0.1:27017/stayfinder'
 main().then(() => {
@@ -28,45 +30,72 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
   
 });
+
+const validateListing = (req, res, next) => {
+  let{error} = listingSchema.validate(req.body);
+   let errMsg =error.details.map((el)=>el.message).join(',');
+   if(error){
+    throw new ExpressError(400, error);
+   }else{
+    next();
+   }
+}
 //index route
-app.get('/listings', async (req, res) => {
+app.get('/listings',wrapAsync( async (req, res) => {
   const allListings= await Listing.find({});
     res.render("listings/index.ejs", {allListings});
-  });
+  })
+);
   //lsings new route
 app.get('/listings/new', (req, res) => {
   res.render('listings/new.ejs');
 });
 //show route
-app.get('/listings/:id',async (req, res) => {
+app.get('/listings/:id',wrapAsync(async (req, res,) => {
   const{id} = req.params;
   const listing= await Listing.findById(id); 
   res.render("listings/show.ejs", {listing});
-});
+}));
 //create route
-app.post('/listings', async (req, res) => {
+app.post('/listings', validateListing,
+   wrapAsync(async (req, res, next) => {
+  
   const newListing = new Listing(req.body);
   await newListing.save();
   res.redirect('/listings');
-});
+})
+);
 //edit route
-app.get('/listings/:id/edit',async (req, res) => {
+app.get('/listings/:id/edit',wrapAsync(async (req, res) => {
   const{id} = req.params;
   const listing = await Listing.findById(id);
   res.render('listings/edit.ejs', {listing});
-});
+}));
 //update route
-app.put('/listings/:id',async (req, res) => {
+app.put('/listings/:id' , validateListing, wrapAsync(async (req, res) => {
   const{id}=req.params;
   const updatedListing = await Listing.findByIdAndUpdate(id, {...req.body});
   res.redirect(`/listings/${updatedListing._id}`); // Redirect to the updated listing's show page
-});
+}));
 //delete route
-app.delete('/listings/:id',async(req,res)=>{
+app.delete('/listings/:id',wrapAsync(async(req,res)=>{
   const {id} = req.params;
   await Listing.findByIdAndDelete(id);
   res.redirect('/listings');
-})
+}));
+
+// Catch-all 404 (works in Express 5)
+app.use((req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Something went wrong";
+  res.status(statusCode).render("error.ejs",{message});
+  // res.status(statusCode).send(message);
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
